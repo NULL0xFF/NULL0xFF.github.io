27c0e9963d7c05f945c1a2e6cacd0eb255e6b9d8
Ji Myoung Ha <NoPlayer40600@gmail.com>
2023-04-24 18:10:24 +0900

    add 'mermaid' support

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,64 +1,62 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'NULL0xFF의 개발 블로그',
-  tagline: 'Docusaurus는 대단해!',
-  favicon: 'img/favicon.ico',
+  title: "NULL0xFF의 개발 블로그",
+  tagline: "Docusaurus는 대단해!",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://null0xff.com',
+  url: "https://null0xff.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'NULL0xFF', // Usually your GitHub org/user name.
-  projectName: 'NULL0xFF.github.io', // Usually your repo name.
+  organizationName: "NULL0xFF", // Usually your GitHub org/user name.
+  projectName: "NULL0xFF.github.io", // Usually your repo name.
   trailingSlash: false,
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'ko',
-    locales: ['ko'],
+    defaultLocale: "ko",
+    locales: ["ko"],
     localeConfigs: {
       ko: {
-        htmlLang: 'ko-KR',
+        htmlLang: "ko-KR",
       },
     },
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/NULL0xFF/NULL0xFF.github.io/tree/main/',
+          editUrl: "https://github.com/NULL0xFF/NULL0xFF.github.io/tree/main/",
         },
         blog: {
-          routeBasePath: '/',
+          routeBasePath: "/",
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/NULL0xFF/NULL0xFF.github.io/tree/main/',
+          editUrl: "https://github.com/NULL0xFF/NULL0xFF.github.io/tree/main/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -69,47 +67,47 @@ const config = {
     ({
       // Replace with your project's social card
       navbar: {
-        title: 'NULL0xFF의 개발 블로그',
+        title: "NULL0xFF의 개발 블로그",
         logo: {
-          alt: '사이트 로고',
-          src: 'img/logo.svg',
+          alt: "사이트 로고",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'documentSideBar',
-            position: 'left',
-            label: '문서',
+            type: "docSidebar",
+            sidebarId: "documentSideBar",
+            position: "left",
+            label: "문서",
           },
           {
-            href: 'https://github.com/NULL0xFF',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/NULL0xFF",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: '사이트맵',
+            title: "사이트맵",
             items: [
               {
-                label: '블로그',
-                to: '/',
+                label: "블로그",
+                to: "/",
               },
               {
-                label: '문서',
-                to: '/docs/intro',
+                label: "문서",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: '기타',
+            title: "기타",
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/NULL0xFF',
+                label: "GitHub",
+                href: "https://github.com/NULL0xFF",
               },
             ],
           },
@@ -121,6 +119,11 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+
+  markdown: {
+    mermaid: true,
+  },
+  themes: ["@docusaurus/theme-mermaid"],
 };
 
 module.exports = config;
